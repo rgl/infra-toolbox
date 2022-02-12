c001aa0fed8d47c36327839cb2edd326d2f4edbc
Rui Lopes <rgl@ruilopes.com>
2022-02-12 09:20:52 +0000

    upgrade to govc 0.27.4

--- provision-govc.sh
+++ provision-govc.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 # see https://github.com/vmware/govmomi/releases
-govc_version='0.27.2'
+govc_version='0.27.4'
 govc_url="https://github.com/vmware/govmomi/releases/download/v$govc_version/govc_Linux_x86_64.tar.gz"
 govc_archive="$(basename "$govc_url")"
 wget -q "$govc_url"
