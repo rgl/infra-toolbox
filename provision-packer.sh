4dfb0128e1e53fbc2379ad7a37f60862d2ae3998
Rui Lopes <rgl@ruilopes.com>
2022-02-12 09:20:39 +0000

    upgrade to packer 1.7.10

--- provision-packer.sh
+++ provision-packer.sh
@@ -2,6 +2,6 @@
 set -euxo pipefail
 
 # see https://github.com/hashicorp/packer/releases
-packer_version='1.7.9'
+packer_version='1.7.10'
 wget -q -O/tmp/packer_${packer_version}_linux_amd64.zip https://releases.hashicorp.com/packer/${packer_version}/packer_${packer_version}_linux_amd64.zip
 unzip /tmp/packer_${packer_version}_linux_amd64.zip -d /usr/local/bin
