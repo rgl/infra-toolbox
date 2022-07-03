ec3acfcd984dbe5be9b9804b76402dda8e3f99a3
Rui Lopes <rgl@ruilopes.com>
2022-07-03 19:17:59 +0100

    download and install from temporary location

--- playbooks/roles/cue/files/install.sh
+++ playbooks/roles/cue/files/install.sh
@@ -1,7 +1,7 @@
 set -euo pipefail
 
 # bail when already installed.
-if [ -f /usr/local/bin/cue ]; then
+if [ -x /usr/local/bin/cue ]; then
     # e.g. cue version v0.4.1 linux/amd64
     actual_version="$(/usr/local/bin/cue version | perl -ne '/^cue version v(.+?) / && print $1')"
     if [ "$actual_version" == "$CUE_VERSION" ]; then
@@ -12,8 +12,7 @@ fi
 
 # download and install.
 cue_url="https://github.com/cue-lang/cue/releases/download/v${CUE_VERSION}/cue_v${CUE_VERSION}_linux_amd64.tar.gz"
-pushd /tmp
-wget -qO- "$cue_url" | tar xzf - cue
-install cue /usr/local/bin/
-rm cue
-popd
+t="$(mktemp -q -d --suffix=.cue)"
+wget -qO- "$cue_url" | tar xzf - -C "$t" cue
+install "$t/cue" /usr/local/bin/
+rm -rf "$t"
--- playbooks/roles/dagger/files/install.sh
+++ playbooks/roles/dagger/files/install.sh
@@ -1,7 +1,7 @@
 set -euo pipefail
 
 # bail when already installed.
-if [ -f /usr/local/bin/dagger ]; then
+if [ -x /usr/local/bin/dagger ]; then
     # e.g. dagger 0.2.6 (e2c2213a) linux/amd64
     actual_version="$(/usr/local/bin/dagger version | perl -ne '/^dagger (.+?) / && print $1')"
     if [ "$actual_version" == "$DAGGER_VERSION" ]; then
@@ -12,8 +12,7 @@ fi
 
 # download and install.
 dagger_url="https://github.com/dagger/dagger/releases/download/v${DAGGER_VERSION}/dagger_v${DAGGER_VERSION}_linux_amd64.tar.gz"
-pushd /tmp
-wget -qO- "$dagger_url" | tar xzf - dagger
-install dagger /usr/local/bin/
-rm dagger
-popd
+t="$(mktemp -q -d --suffix=.dagger)"
+wget -qO- "$dagger_url" | tar xzf - -C "$t" dagger
+install "$t/dagger" /usr/local/bin/
+rm -rf "$t"
